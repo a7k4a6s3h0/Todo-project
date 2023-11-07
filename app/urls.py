e789f197ca70477dc2d5afd4443d98db520b9b83
a7k4a6s3h0 <akashsuresh267@gmail.com>
2023-11-07 16:42:56 +0530

    second

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
+# <..........................FUNCTION BASED VIEWS URLS..............................>
+
+# urlpatterns = [
     
-    path('',views.main_view, name='home'),
-    path('update_task/<str:slug>/', views.Task_update_View, name='update_task'),
-    path('get/ajax/del_task', views.Delete_Task, name='del_task')
+#     path('',views.main_view, name='home'),
+#     path('update_task/<str:slug>/', views.Task_update_View, name='update_task'),
+#     path('get/ajax/del_task', views.Delete_Task, name='del_task')
+# ]
+
+
+
+# <..........................CLASS BASED VIEWS URLS..............................>
+
+urlpatterns = [
+    path('',views.HomeView.as_view(), name='home'),
+    path('update_task/<str:slug>/', views.UpdateTaskView.as_view(), name='update_task'),
+    path('get/ajax/del_task', views.DeleteTaskView.as_view(), name='del_task')
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
 from django.contrib import messages
+from django.urls import reverse_lazy
+from django.views import generic, View
 from .dj_forms import *
 from . models import *
 
@@ -48,3 +50,53 @@ def Delete_Task(request):
         "status": 201,
         "message":"Task deleted successfully"
     })
+
+
+
+# <.......................CLASS BASED VIEWS.............................>
+
+
+class HomeView(generic.ListView):
+
+    model = User_Tasks
+    template_name = 'home.html'
+    context_object_name = 'task_instance'
+    ordering = ['-created_at']
+
+    def post(self, request):
+        
+        form = CreateTaskForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Task Created Successfully...!!!')
+            return redirect('/')
+        else:
+            print("form is not valid", form.errors)
+            messages.error(request,'something went worng...!!!')
+
+        return super().get(request)        
+    
+class UpdateTaskView(generic.UpdateView):
+
+    model = User_Tasks
+    template_name = 'update.html'
+    fields = ['task_name', 'task_describtion']
+    context_object_name = 'task_instance'
+    success_url = reverse_lazy('home')  
+
+    def form_valid(self, form):
+        old_name = self.get_object().task_name
+        messages.success(self.request, f'Task {old_name} Updated Successfully...!!!')
+        return super().form_valid(form)
+    
+class DeleteTaskView(View):
+
+    def get(self, request):
+            slug = request.GET.get('value')
+            task_instance = get_object_or_404(User_Tasks, slug=slug)
+            task_instance.delete()
+            return JsonResponse({
+                "status": 201,
+                "message": "Task deleted successfully"
+            })
